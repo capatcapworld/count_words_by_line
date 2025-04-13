ab61b5cd33ad672a8125cfafb02e6f741f1d11d4
Carsten Pedersen <carsten.pedersen@capworld.dk>
2025-04-13 15:16:18 +0200

    Minor changes

--- src/main/java/com/capworld/WordCounter.java
+++ src/main/java/com/capworld/WordCounter.java
@@ -4,11 +4,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.PriorityQueue;
 
 public class WordCounter {
 
-    public void start() {
+    public void calc() {
         Map<String, Integer> numberOfFoundWords = new HashMap<>();
 
         ClassLoader classLoader = this.getClass().getClassLoader();
@@ -27,10 +32,9 @@ public class WordCounter {
                 }
             }
 
-            // Use a priority queue to get top entries
+            // Use a priority queue to contain the top words
             PriorityQueue<Map.Entry<String, Integer>> topWords = new PriorityQueue<>(
-                    Map.Entry.comparingByValue(Integer::compare)
-
+                    Map.Entry.comparingByValue(Integer::compare) // Order of priorityQueue
             );
 
             for (Map.Entry<String, Integer> wordWithCounts : numberOfFoundWords.entrySet()) {
